2acf0dfbfcda9ec8a7c7a0daf8671a54dafbd585
acaztec <acrinion@aztecsoftware.com>
2025-08-15 11:16:50 -0500

    Fix React import for useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import ScholarshipGrid from './components/ScholarshipGrid';
 import FilterSidebar from './components/FilterSidebar';
